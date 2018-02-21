fd39d1e7c53c30472c36df56902361cb7794075c
Sander Jochems <sanderjochems@hotmail.nl>
2018-02-21 14:01:09 +0100

    Update head.php

--- Website/assets/core/head.php
+++ Website/assets/core/head.php
@@ -7,6 +7,7 @@
     <title><?php if (isset($pageTitle)) { echo $pageTitle; } else { echo "Pagina"; } ?> | Online Stemmen</title>
 
     <link rel="stylesheet" type="text/css" href="/assets/vendor/style.css">
+    <link rel="stylesheet" type="text/css" href="/assets/vendor/fontawesome-5.0.6/css/fontawesome-all.min.css">
   </head>
   <body>
 <!-- Navbar -->
