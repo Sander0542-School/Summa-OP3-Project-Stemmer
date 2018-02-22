ba6dca35533079d6cafd70104f1fce30f17106fa
Sander Jochems <sanderjochems@hotmail.nl>
2018-02-22 09:58:40 +0100

    Update content.php

---
+++ Website/assets/core/foot/content.php
@@ -0,0 +1,27 @@
+
+<!-- Footer -->
+
+    <div class="footer">
+      <div class="footer-content">
+        <div class="content-left">
+          <h2 class="footer-title">Online Stemmen. Voor Nederland</h2>
+        </div>
+        <div class="content-right">
+          <div class="content-row">
+            <h2>Service</h2>
+            <ul>
+              <li>
+                <a href="/contact">Contact</a>
+              </li>
+              <li>
+                <a href="/sitemap">Sitemap</a>
+              </li>
+            </ul>
+          </div>
+          <div class="content-row">
+            <h2>Over deze site</h2>
+          </div>
+        </div>
+      </div>
+    </div>
+
