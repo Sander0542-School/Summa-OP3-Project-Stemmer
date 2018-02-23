4beca8781274a2defb8b8dee89540fe371b43450
Sander Jochems <sanderjochems@hotmail.nl>
2018-02-23 12:31:03 +0100

    Update stemmen.php

--- Website/stemmen.php
+++ Website/stemmen.php
@@ -4,5 +4,27 @@ include "assets/core/head.php";
 ?>
 
 <?php
+if (isset($_POST["loginBsn"]) && isset($_POST["loginCode"])) {
+  if ($CORE->login($_POST["loginBsn"], $_POST["loginCode"])) {
+    echo '<div class="messagebox green"><h2>U bent succesvol ingelogd</h2></div>';
+  } else {
+    echo '<div class="messagebox red"><h2>U kunt niet inloggen met deze BSN en Code</h2></div>';
+  }
+}
+
+if ($CORE->isIngelogd()) {
+  echo 'Halloottjesss';
+} else {
+  echo '
+    <form action="/stemmen" method="post">
+      <input type="text" placeholder="bsn" name="loginBsn">
+      <input type="text" placeholder="code" name="loginCode">
+      <input type="submit" value="login">
+    </form>
+';
+}
+?>
+
+<?php
 include "assets/core/foot.php";
 ?>
