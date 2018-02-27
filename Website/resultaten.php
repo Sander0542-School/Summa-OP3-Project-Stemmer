7f9ab0fcebb1199db9d80513db75272b58b17f05
Bram Swinkels <33626711+Bsrawm@users.noreply.github.com>
2018-02-27 13:10:21 +0100

    Update 2 Files

--- Website/resultaten.php
+++ Website/resultaten.php
@@ -16,6 +16,43 @@ include "assets/core/head.php";
             }
         ?>
     </select>
-<?php
+    <html>
+
+    <head>
+        <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+        <script type="text/javascript">
+            google.charts.load('current', {
+                'packages': ['corechart']
+            });
+            google.charts.setOnLoadCallback(drawChart);
+
+            function drawChart() {
+
+                var data = google.visualization.arrayToDataTable([
+                    ['Stemmen', 'Stemmen per gemeente'],
+                    ['CDA', 11],
+                    ['PVV', 2],
+                    ['VVD', 2],
+                    ['GroenLinks', 2],
+                    ['D66', 7]
+                ]);
+
+                var options = {
+                    title: 'Stemmen per gemeente'
+                };
+
+                var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+
+                chart.draw(data, options);
+            }
+        </script>
+    </head>
+
+    <body>
+        <div id="piechart" style="width: 900px; height: 500px;"></div>
+    </body>
+
+    </html>
+    <?php
 include "assets/core/foot.php";
 ?>
