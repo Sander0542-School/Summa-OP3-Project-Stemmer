0ef2e1d3e2b8a7a9ccdd8bde60479a66d750597b
Bram Swinkels <33626711+Bsrawm@users.noreply.github.com>
2018-02-27 12:24:24 +0100

    Update resultaten.php

--- Website/resultaten.php
+++ Website/resultaten.php
@@ -3,6 +3,19 @@ $pageTitle = "Resultaten";
 include "assets/core/head.php";
 ?>
 
+    <select>
+        <?php
+            $gemeenten = $CORE->krijgGemeenten();
+            if ($gemeenten) {
+                echo '
+        <option value="0" disable>Selecteer een gemeente</option>';
+                foreach ($gemeenten as $gemeente) {
+                    echo '
+        <option value="'.$gemeente["id"].'">'.$gemeente["naam"].'</option>';
+                }
+            }
+        ?>
+    </select>
 <?php
 include "assets/core/foot.php";
 ?>
