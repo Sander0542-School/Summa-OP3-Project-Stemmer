a3620a7d60a48661ed09507b39cb9b96b7d175cc
Sander Jochems <sanderjochems@hotmail.nl>
2018-03-02 12:30:59 +0100

    Update 5 Files
    Finish the WPF Application

---
+++ WPF/StemResultaten/DatabaseConnection.cs
@@ -0,0 +1,51 @@
+﻿using MySql.Data.MySqlClient;
+
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StemResultaten
+{
+    class DatabaseConnection
+    {
+        private MySqlConnection connection = new MySqlConnection("Server=localhost;Database=stemmen;Uid=root;Pwd=;");
+
+        public DataTable krijgGemeenten()
+        {
+            connection.Open();
+
+            MySqlCommand sqlCommand = connection.CreateCommand();
+            sqlCommand.CommandText = "SELECT id, naam FROM gemeenten";
+
+            MySqlDataReader dataReader = sqlCommand.ExecuteReader();
+
+            DataTable dataTable = new DataTable();
+            dataTable.Load(dataReader);
+
+            connection.Close();
+
+            return dataTable;
+        }
+
+        public DataTable krijgStemmenPerGemeente(int iGemeenteId)
+        {
+            connection.Open();
+
+            MySqlCommand sqlCommand = connection.CreateCommand();
+            sqlCommand.CommandText = "SELECT COUNT(stemmen.uID) as stemmen, partijen.naam as partijNaam, partijen.afkorting as partijAfkorting FROM stemmen INNER JOIN partijen ON stemmen.partij = partijen.id INNER JOIN gebruikers ON stemmen.uID = gebruikers.id INNER JOIN gemeenten ON gebruikers.gemeente = gemeenten.id WHERE gemeenten.id = @gemeenteId AND partijen.gemeente = @gemeenteId GROUP BY stemmen.partij";
+            sqlCommand.Parameters.AddWithValue("@gemeenteId", iGemeenteId);
+
+            MySqlDataReader dataReader = sqlCommand.ExecuteReader();
+
+            DataTable dataTable = new DataTable();
+            dataTable.Load(dataReader);
+
+            connection.Close();
+
+            return dataTable;
+        }
+    }
+}
--- WPF/StemResultaten/MainWindow.xaml.cs
+++ WPF/StemResultaten/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +21,45 @@ namespace StemResultaten
     /// </summary>
     public partial class MainWindow : Window
     {
+        private DatabaseConnection dbConnection = new DatabaseConnection();
+
         public MainWindow()
         {
             InitializeComponent();
+
+            updateComboBoxGemeenten();
+        }
+
+        private void updateComboBoxGemeenten()
+        {
+            cbGemeenten.Items.Clear();
+
+            DataTable dataTable = dbConnection.krijgGemeenten();
+
+            DataRow dataRow = dataTable.NewRow();
+            dataRow[0] = 0;
+            dataRow[1] = "Selecteer een Gemeente";
+
+            dataTable.Rows.InsertAt(dataRow, 0);
+
+            cbGemeenten.ItemsSource = dataTable.DefaultView;
+
+            cbGemeenten.SelectedIndex = 0;
+        }
+
+        private void updateChart(int iGemeenteId)
+        {
+            dgStemResultaten.ItemsSource = dbConnection.krijgStemmenPerGemeente(iGemeenteId).DefaultView;
+        }
+
+        private void cbGemeenten_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ComboBox comboBox = (ComboBox)sender;
+
+            if (comboBox.SelectedIndex != 0)
+            {
+                updateChart(comboBox.SelectedIndex);
+            }
         }
     }
 }
