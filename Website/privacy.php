36834f61dd79dba318259c1e4c9c52cf35509b24
Bram Swinkels <33626711+Bsrawm@users.noreply.github.com>
2018-03-20 10:57:32 +0100

    Fixed normaal Nederlands

--- Website/privacy.php
+++ Website/privacy.php
@@ -5,20 +5,20 @@ include "assets/core/head.php";
     <div class="information-content">
         <h1>Privacy</h1>
         <div class="inleiding">
-            <p>Persoonsgegevens die u op deze website opgeeft, gebruikt de Onlinestemmen alleen voor het doel waarvoor u ze
+            <p>Persoonsgegevens die u op deze website opgeeft, gebruikt Onlinestemmen alleen voor het doel waarvoor u ze
                 heeft gegeven.</p>
             <br>
         </div>
 
-        <b>Hoe gaat de Onlinestemmen om met uw persoonsgegevens?</b>
+        <b>Hoe gaat Onlinestemmen om met uw persoonsgegevens?</b>
         <br>
         <br>
         <div class="content">
             <p>Wilt u een brochure aanvragen? Of een nieuwsbrief ontvangen? Dan hebben wij uw persoonsgegevens, zoals uw naam,
-                e-mailadres of woonadres nodig. Bij het aanvraagformulier staat vermeld welke gegevens de Onlinestemmen gebruikt
+                e-mailadres of woonadres nodig. Bij het aanvraagformulier staat vermeld welke gegevens Onlinestemmen gebruikt
                 en waarvoor.</p>
-            <br> De Onlinestemmen gebruikt uw persoonsgegevens alleen voor het doel waarvoor u ze heeft achtergelaten. Heeft
-            u bijvoorbeeld uw naam en adres ingevuld om een brochure te ontvangen? Dan gebruikt de Onlinestemmen uw gegevens
+            <br> Onlinestemmen gebruikt uw persoonsgegevens alleen voor het doel waarvoor u ze heeft achtergelaten. Heeft
+            u bijvoorbeeld uw naam en adres ingevuld om een brochure te ontvangen? Dan gebruikt Onlinestemmen uw gegevens
             alleen om u die brochure toe te sturen en dus niet voor het toesturen van andere brochures.</p>
             <br>
         </div>
@@ -26,7 +26,7 @@ include "assets/core/head.php";
         <br>
         <br>
         <div class="content">
-            <p>De Onlinestemmen kan gegevens wel gebruiken voor opsporing. Bijvoorbeeld als iemand via de site strafbare feiten
+            <p>Onlinestemmen kan gegevens wel gebruiken voor opsporing. Bijvoorbeeld als iemand via de site strafbare feiten
                 pleegt of strafbare uitlatingen doet.</p>
             <br>
         </div>
@@ -34,7 +34,7 @@ include "assets/core/head.php";
         <br>
         <br>
         <div class="content">
-            <p>De Onlinestemmen bewaart persoonsgegevens niet langer dan nodig is voor het doel waarvoor u ze heeft achtergelaten.
+            <p>Onlinestemmen bewaart persoonsgegevens niet langer dan nodig is voor het doel waarvoor u ze heeft achtergelaten.
                 Daarna worden de gegevens vernietigd.</p>
             <br>
         </div>
@@ -42,7 +42,7 @@ include "assets/core/head.php";
         <br>
         <br>
         <div class="content">
-            <p>De Onlinestemmen gebruikt uw persoonsgegevens alleen voor het doel waarvoor u ze heeft achtergelaten. Voor onze
+            <p>Onlinestemmen gebruikt uw persoonsgegevens alleen voor het doel waarvoor u ze heeft achtergelaten. Voor onze
                 dienstverlening via social media maken wij echter gebruik van externe platforms, zoals Twitter en WhatsApp.
                 Wij vinden het belangrijk om te melden dat wij geen invloed hebben op de wijze waarop deze platforms omgaan
                 met uw persoonsgegevens. Wij adviseren u dan ook geen privacygevoelige gegevens te delen via deze platforms.
