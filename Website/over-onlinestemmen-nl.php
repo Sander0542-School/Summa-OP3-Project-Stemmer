ea9b9c0ae4567ef3f9ba5a3377f499826bc1337b
Sander Jochems <sanderjochems@hotmail.nl>
2018-03-20 09:06:06 +0100

    Update over-onlinestemmen-nl.php

--- Website/over-onlinestemmen-nl.php
+++ Website/over-onlinestemmen-nl.php
@@ -3,50 +3,50 @@ $pageTitle = "Over Onlinestemmen.nl";
 include "assets/core/head.php";
 ?>
     <div class="information-content">
-        <h1>Over Rijksoverheid.nl</h1>
+        <h1>Over Onlinestemmen.nl</h1>
         <div class="inleiding">
-            <p>Op Rijksoverheid.nl informeren ministeries u over wetten en regels van het Rijk.</p>
+            <p>Op Onlinestemmen.nl informeren ministeries u over wetten en regels van het Rijk.</p>
             <br>
         </div>
-        <b>Welke informatie vindt u op Rijksoverheid.nl?</b>
+        <b>Welke informatie vindt u op Onlinestemmen.nl?</b>
         <br>
         <div class="content">
-            <p>De website Rijksoverheid.nl is de gezamenlijke website van de 12 ministeries. Ministeries houden zich onder andere
-                bezig met wetsvoorstellen, regels en beleidsplannen maken. Op Rijksoverheid.nl informeren we u hierover.
+            <p>De website Onlinestemmen.nl is de gezamenlijke website van de 12 ministeries. Ministeries houden zich onder andere
+                bezig met wetsvoorstellen, regels en beleidsplannen maken. Op Onlinestemmen.nl informeren we u hierover.
                 Wij kondigen bijvoorbeeld aan als een belangrijk wetsvoorstel naar de Tweede Kamer is gestuurd. En als de
-                wet is aangenomen, leggen we op Rijksoverheid.nl uit wat de wet voor u betekent.
+                wet is aangenomen, leggen we op Onlinestemmen.nl uit wat de wet voor u betekent.
             </p>
 
-            <p>U vindt op Rijksoverheid.nl bijvoorbeeld antwoorden op deze vragen: wat is het minimumloon, wanneer heb ik recht
+            <p>U vindt op Onlinestemmen.nl bijvoorbeeld antwoorden op deze vragen: wat is het minimumloon, wanneer heb ik recht
                 op huurtoeslag of wat doe ik bij een noodgeval in het buitenland? Maar ook: hoe stimuleert de Rijksoverheid
                 duurzame energie of wat doet de overheid tegen huiselijk geweld? De antwoorden op deze vragen vindt u via
                 de onderwerpen.
             </p>
 
-            <p>Daarnaast vindt u op Rijksoverheid.nl informatie over de regering en het kabinet. Het kabinet bestaat uit alle
+            <p>Daarnaast vindt u op Onlinestemmen.nl informatie over de regering en het kabinet. Het kabinet bestaat uit alle
                 ministers en alle staatssecretarissen. Lees bijvoorbeeld wat de plannen van het kabinet zijn in het regeerakoord.
             </p>
             <br>
         </div>
-        <b>Staat er ook informatie van andere overheden op Rijksoverheid.nl?</b>
+        <b>Staat er ook informatie van andere overheden op Onlinestemmen.nl?</b>
         <div class="content">
-            <p>Nee, u vindt op Rijksoverheid.nl geen informatie van andere overheden. Alle informatie is afkomstig van de ministeries.
+            <p>Nee, u vindt op Onlinestemmen.nl geen informatie van andere overheden. Alle informatie is afkomstig van de ministeries.
                 Zo vindt u hier bijvoorbeeld geen informatie van gemeenten. Of van uitvoeringsorganisaties die onder ministeries
                 vallen. Bijvoorbeeld de Dienst Uitvoering Onderwijs (DUO), de Belastingdienst of Rijkswaterstaat. Zij hebben
-                hun eigen websites. Rijksoverheid.nl helpt u op weg naar informatie van deze organisaties. Ook verwijst Rijksoverheid.nl
+                hun eigen websites. Onlinestemmen.nl helpt u op weg naar informatie van deze organisaties. Ook verwijst Onlinestemmen.nl
                 u door naar overheidssites over bepaalde thema’s. Zoals de website
-                <a href="https://www.ondernemersplein.nl/">Ondernemersplein.nl</a> over subsidies, regels en vergunningen voor bedrijven. Rijksoverheid.nl is niet verantwoordelijk
+                <a href="https://www.ondernemersplein.nl/">Ondernemersplein.nl</a> over subsidies, regels en vergunningen voor bedrijven. Onlinestemmen.nl is niet verantwoordelijk
                 voor de inhoud en actualiteit van deze websites.
                 <br>
                 <br>Bijvoorbeeld:
-                <br>Een paspoort of identiteitskaart vraagt u aan bij de gemeente. Op Rijksoverheid.nl leest u de regels die
+                <br>Een paspoort of identiteitskaart vraagt u aan bij de gemeente. Op Onlinestemmen.nl leest u de regels die
                 daarvoor gelden. Bijvoorbeeld het maximumtarief dat de gemeente bij u in rekening mag brengen. Wilt u een
-                paspoort aanvragen? Dan verwijst Rijksoverheid.nl u door naar uw gemeente.</p>
+                paspoort aanvragen? Dan verwijst Onlinestemmen.nl u door naar uw gemeente.</p>
             <br>
         </div>
         <b>Wat is het verschil met Overheid.nl?</b>
         <div class="content">
-            <p>Op Rijksoverheid.nl worden beleidsplannen, wetten en regels uitgelegd voor burgers, bedrijven en journalisten.
+            <p>Op Onlinestemmen.nl worden beleidsplannen, wetten en regels uitgelegd voor burgers, bedrijven en journalisten.
                 Op de website
                 <a href="https://www.overheid.nl/overheidsinformatie">Overheid.nl</a> staan de originele wetteksten van de
                 <a href="http://wetten.overheid.nl/zoeken/">verdragen, wetten en regels</a> die in Nederland gelden. Ook vindt u er bijvoorbeeld vergunningen die zijn
@@ -54,9 +54,9 @@ include "assets/core/head.php";
                 <p>
         </div>
         <br>
-        <b>Contact met Rijksoverheid.nl</b>
+        <b>Contact met Onlinestemmen.nl</b>
         <div class="content">
-            <br> De website Rijksoverheid.nl wordt beheerd door het ministerie van Algemene Zaken. Heeft u een vraag over de
+            <br> De website Onlinestemmen.nl wordt beheerd door het ministerie van Algemene Zaken. Heeft u een vraag over de
             website? Dan kunt u
             <a href="/contact">contact opnemen</a> met het loket Informatie Rijksoverheid.
         </div>
